1cba4f6b4b6a1da91135d7d62ae080936f0bafa0
Jesus Guio <jfranciscoguio@protonmail.com>
2021-06-03 16:31:59 -0500

    [Game] Add Animations controler

--- scripts/game/drawData.lua
+++ scripts/game/drawData.lua
@@ -13,3 +13,51 @@ function drawData.debugWindow()
     imgui.text("1 / 60: "..defaultFormat(1 / 60.0))
     imgui.text("Application average "..defaultFormat(1000.0 / frameRate).." ms/frame ("..defaultFormat(frameRate).." FPS)")
 end
+
+local selectedIdle = true
+local selectedAttack = false
+local selectedDeath = false
+local selectedWalk = false
+local currentAnimationType = components.AnimationType.idle
+function drawData.animationsControl()
+    selectedIdle = imgui.checkBox("Idle", selectedIdle)
+    if (selectedIdle) then
+        selectedAttack = false
+        selectedDeath = false
+        selectedWalk = false
+        currentAnimationType = components.AnimationType.idle
+    end
+
+    selectedAttack = imgui.checkBox("Attack", selectedAttack)
+    if (selectedAttack) then
+        selectedIdle = false
+        selectedDeath = false
+        selectedWalk = false
+        currentAnimationType = components.AnimationType.attack
+    end
+
+    selectedDeath = imgui.checkBox("Death", selectedDeath)
+    if (selectedDeath) then
+        selectedIdle = false
+        selectedAttack = false
+        selectedWalk = false
+        currentAnimationType = components.AnimationType.death
+    end
+
+    selectedWalk = imgui.checkBox("Walk", selectedWalk)
+    if (selectedWalk) then
+        selectedIdle = false
+        selectedAttack = false
+        selectedDeath = false
+        currentAnimationType = components.AnimationType.walk
+    end
+
+    for i = 1, scene.entities:size() do
+        local entity = scene.entities:get(i)
+
+        if entity.components & components.type.animation then
+            local animations = scene.components.getAnimation(entity.id)
+            animations.currentType = currentAnimationType
+        end
+    end
+end
